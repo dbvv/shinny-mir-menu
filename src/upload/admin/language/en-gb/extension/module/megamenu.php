f14d89ab47f835aa1fd2756f0ac860c5b4dbed94
Daniil Borovkov <daniilborowkow@gmail.com>
2019-10-15 08:26:06 +0600

    WIP copy admin template. Setup

---
+++ src/upload/admin/language/en-gb/extension/module/megamenu.php
@@ -0,0 +1,73 @@
+<?php
+// Heading 
+$_['heading_title']     		= 'OCtemplates Megamenu';
+
+
+// Text
+$_['text_th_title']				= 'Title';
+$_['text_th_link']				= 'Link';
+$_['text_th_sort_order']		= 'Sort Order';
+
+$_['text_title']				= 'Title';
+$_['text_date']					= 'Date added';
+
+
+$_['text_action']				= 'Acrtion';
+$_['text_status']				= 'Status';
+$_['text_no_results']			= 'Empty';
+$_['text_list']    				= 'List';
+$_['text_edit']             	= 'Edit';
+$_['text_module']               = 'Modules';
+$_['text_success']              = 'The Megamenu module has been successfully changed!';
+$_['text_edit']                 = 'Edit';
+$_['tab_general']               = 'Main';
+$_['tab_html']                  = 'HTML-block';
+$_['tab_options']               = 'Options';
+
+$_['text_category']             = 'Categories';
+
+$_['text_type']             	= 'Menu type';
+$_['text_type_category']        = 'Category';
+$_['text_type_html']            = 'Static html block';
+$_['text_type_link']            = 'Link';
+$_['text_type_manufacturer']    = 'Manufacturer';
+
+$_['text_manufacturer']         = 'Manufacturer';
+$_['text_type_information']     = 'Information';
+$_['text_information']          = 'Information';
+$_['text_type_product']         = 'Product';
+$_['text_product']             	= 'Products';
+$_['text_link_options']         = 'Open link in new window?';
+$_['text_link_options_yes']     = 'Yes';
+$_['text_link_options_no']      = 'No';
+$_['text_product_width']        = 'Image Width';
+$_['text_product_height']       = 'Image Height';
+$_['text_type_auth']            = 'Authorization';
+$_['text_html_description']     = 'Text';
+$_['text_add_html']             = 'Use HTML-block';
+$_['tab_add_html']             	= 'HTML-block';
+$_['text_thumb']             	= 'Image';
+
+
+$_['text_variant_category']             	 = 'Dropdown list view';
+$_['variant_category_simple']             	 = 'Simple list';
+$_['variant_category_full']             	 = 'Full width of the layout';
+$_['variant_category_full_image']            = 'Full width of the layout (with images)';
+$_['text_link']                              = 'Link';
+$_['text_category_show_subcategory']         = 'Show subcategories';
+$_['text_sort_order']                        = 'Sort Order';
+$_['text_limit_item']                        = 'Sub-category limit';
+
+
+// Entry
+$_['entry_status_module']     = 'Module Status';
+
+// Error
+$_['error_permission'] = 'Attention: There is not enough rights to change the module to Megamenu!';
+$_['error_title']  = 'Field Link references is required!';
+$_['error_menu_type']  = 'Field Menu type is required!';
+$_['error_html']  = 'Field Text is required!';
+
+
+
+?>
